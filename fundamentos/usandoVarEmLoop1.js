e8dc74fd9ace30633937df61b64e7eaddac3fe77
Felipe Duran <felipe.duran@perugluglu.net>
2021-04-01 22:34:42 -0300

    fundamentos

---
+++ fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 10; i++){
+    console.log(i)
+}
+console.log('i =', i)
+
+// for 
+        //var i = 0; declarando a variavel
+        //i < 10; condição para repetição
+        //i++ incremento
